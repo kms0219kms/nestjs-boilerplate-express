d5fdf2f9d11d248187a829c7b57c00d55b845026
Minsu Kim <kms0219kms@naver.com>
2025-01-10 17:02:25 +0000

    ✨ feat: 보일러플레이트 최신화

--- src/common/filters/global-exception.filter.ts
+++ src/common/filters/global-exception.filter.ts
@@ -4,28 +4,32 @@ import {
   ArgumentsHost,
   HttpException,
   HttpStatus,
-} from '@nestjs/common';
+  Logger,
+} from '@nestjs/common'
 
-import APIException from '../dto/APIException.dto';
+import APIException from '../dto/APIException.dto'
 
-import { Response } from 'express';
+import { Response } from 'express'
 
 // HttpException, APIException ...
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(GlobalExceptionFilter.name)
+
   catch(exception: any, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
+    this.logger.error(exception)
+    const ctx = host.switchToHttp()
+    const response = ctx.getResponse<Response>()
 
-    const responseAt: string = new Date().toISOString();
+    const responseAt: string = new Date().toISOString()
 
-    let status: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    let status: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR
     let message: string | object | APIException =
-      '내부 서버 오류가 발생했습니다.';
+      '내부 서버 오류가 발생했습니다.'
 
     if (exception instanceof HttpException) {
-      status = exception.getStatus();
-      message = exception.getResponse();
+      status = exception.getStatus()
+      message = exception.getResponse()
     }
 
     if (exception instanceof APIException) {
@@ -36,8 +40,8 @@ export class GlobalExceptionFilter implements ExceptionFilter {
         data: exception.data,
         message: exception.message,
         responseAt: responseAt,
-      });
-      return;
+      })
+      return
     }
 
     response.status(status).send({
@@ -46,6 +50,6 @@ export class GlobalExceptionFilter implements ExceptionFilter {
 
       message: message['message'] || message['error'] || message,
       responseAt: responseAt,
-    });
+    })
   }
 }
--- src/common/interceptors/transform.interceptor.ts
+++ src/common/interceptors/transform.interceptor.ts
@@ -4,22 +4,22 @@ import {
   ExecutionContext,
   CallHandler,
   HttpStatus,
-} from '@nestjs/common';
+} from '@nestjs/common'
 
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 
-import type { Request, Response } from 'express';
+import type { Request, Response } from 'express'
 
 @Injectable()
 export class TransformInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((data) => {
-        const request = context.switchToHttp().getRequest<Request>();
-        const response = context.switchToHttp().getResponse<Response>();
+        const request = context.switchToHttp().getRequest<Request>()
+        const response = context.switchToHttp().getResponse<Response>()
 
-        if (request.url === '/') return data;
+        if (request.url === '/') return data
 
         return {
           code: response.statusCode.toString().startsWith('2')
@@ -30,8 +30,8 @@ export class TransformInterceptor implements NestInterceptor {
           data: data,
 
           responseAt: new Date().toISOString(),
-        };
+        }
       }),
-    );
+    )
   }
 }
